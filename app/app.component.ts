e0c8279fc0f7214d75d9b13ea3dd34a509eaa0bc
DuyguKeskek <duygukeskek@hotmail.com>
2018-04-16 01:33:14 +0300

    Created task model class and its properties

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 
+import { Task } from './model/task';
+
 @Component ({
     moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html'
 })
 
-export class AppComponent { }
+export class AppComponent { 
+    private tasks = [
+        new Task(
+            "Do some exercise",
+            false
+        ),
+        new Task(
+            "Read a book",
+            false
+        )
+    ]
+}
 
 
